1d9fe3f66a58a6732609afd1579d32fa00eb2fe9
Bob Kozdemba <bkozdemba@gmail.com>
2022-12-11 10:39:59 -0600

    updated

--- triton/client.py
+++ triton/client.py
@@ -1,10 +1,10 @@
-# import json
-import numpy
+import numpy as np
 import requests
 import cv2
 import ast
+import logging
 
-def make_prediction(img:numpy.array, img_size:int, host:str)-> requests:
+def make_prediction(img:np.array, img_size:int, host:str)-> requests:
   """
   Make a binary prediction from a single fingerprint image.
   Args:
@@ -12,8 +12,6 @@ def make_prediction(img:numpy.array, img_size:int, host:str)-> requests:
    img_size - The image height and width.
    host - The hostname[:port] of the model service.
   Returns: The model output prediction.
-
-  Consider passing in the image filename vs. an array?
   """
   req2 = {
       "inputs": [
@@ -30,33 +28,45 @@ def make_prediction(img:numpy.array, img_size:int, host:str)-> requests:
   r = requests.post(url, json=req2)
   return r
 
-#
-# Get the model server info.
-#
-host = "http://fingerprint-sandbox.apps.hou.edgelab.online"
-url = f'{host}/v2'
-r = requests.get(url)
-print(f'REST GET response = {r}')
-print(f'REST GET content = {r.content.decode()}')
-print()
+if __name__ == "__main__":
 
-#
-# Make 4 single image predictions.
-#
-img_size = 96
-datadir = '../data/fingerprint_real'
-filenames = [\
-  '103__F_Left_index_finger.BMP',\
+  logging.basicConfig(level=logging.INFO)
+
+  #
+  # Get the model server info.
+  #
+  host = "http://fingerprint-sandbox.apps.hou.edgelab.online"
+  url = f'{host}/v2'
+  r = ""
+  try:
+    r = requests.get(url)
+    # logging.info("")
+    # logging.info(f'Triton Server Status:')
+    # logging.info("")
+    # logging.info(f'{r.content.decode()}')
+    # logging.info("")
+  except:
+    logging.error(f"Requests Error! {r}")
+
+  #
+  # Make 4 single image predictions.
+  #
+  img_size = 96
+  filepath = '../data/fingerprint_real'
+  filenames = [
+    '103__F_Left_index_finger.BMP',
     '275__F_Left_index_finger.BMP',
     '232__M_Right_index_finger.BMP',
     '504__M_Right_index_finger.BMP'
     ]
-for filename in filenames:
-  F_Left_index = cv2.imread(f'{datadir}/{filename}', cv2.IMREAD_GRAYSCALE)
-  img_resize = cv2.resize(F_Left_index, (img_size, img_size))
-  img_resize.resize(1, img_size, img_size, 1)
-  r = make_prediction(img_resize, img_size, host)
-  print(f'REST inference response = {r}')
-  # print(f'REST inference content = {r.content}')
-  p = ast.literal_eval(r.content.decode())
-  print(f"Image = {filename}, Prediction = {p['outputs'][0]['data']}")
+  for filename in filenames:
+    F_Left_index = cv2.imread(f'{filepath}/{filename}', cv2.IMREAD_GRAYSCALE)
+    img_resize = cv2.resize(F_Left_index, (img_size, img_size))
+    img_resize.resize(1, img_size, img_size, 1)
+    try:
+      r = make_prediction(img_resize, img_size, host)
+      logging.debug(f'REST inference response = {r}')
+      p = ast.literal_eval(r.content.decode())
+      logging.info(f"Fingerprint Image = {filename}, Prediction = {p['outputs'][0]['data']}")
+    except:
+      logging.error(f"Requests POST Error {r}")
